6e0fa0fb4ed53a4938bf0a175c3a2e9e7a6d47c8
Deniz Tanriverdi <deniz.tanriverdi@std.izu.edu.tr>
2022-08-08 17:16:54 +0300

    --comm

---
+++ ConsoleCoreDocker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleCoreDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 1000)
+            {
+                Console.WriteLine("Hello word");
+                i++;
+                System.Threading.Thread.Sleep(1000);
+                //elimde bir puliah olduğu için .net core runtime lazım
+            }
+        }
+    }
+}
